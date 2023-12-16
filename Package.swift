482fb145389a3842b0a44eabc5a592e2385f5384
ensan-hcl <ensan.hcl.dev@gmail.com>
2023-12-17 01:03:47 +0900

    add header

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     products: [
         .library(name: "llama", targets: ["llama"]),
+        .library(name: "grammar_parser", targets: ["grammar-parser"]),
     ],
     targets: [
         .target(
@@ -25,7 +26,6 @@ let package = Package(
                 "ggml-backend.c",
                 "ggml-quants.c",
                 "ggml-metal.m",
-                "common/grammar-parser.cpp",
             ],
             resources: [
                 .process("ggml-metal.metal")
@@ -45,7 +45,32 @@ let package = Package(
             linkerSettings: [
                 .linkedFramework("Accelerate")
             ]
+        ),
+        .target(
+            name: "grammar-parser",
+            path: ".",
+            exclude: [],
+            sources: [
+                "common/grammar-parser.cpp",
+            ],
+            resources: [],
+            publicHeadersPath: "spm-headers",
+            cSettings: [
+                .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
+                .define("GGML_USE_ACCELERATE"),
+                .unsafeFlags(["-fno-objc-arc"]),
+                .define("GGML_USE_METAL"),
+                // NOTE: NEW_LAPACK will required iOS version 16.4+
+                // We should consider add this in the future when we drop support for iOS 14
+                // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
+                // .define("ACCELERATE_NEW_LAPACK"),
+                // .define("ACCELERATE_LAPACK_ILP64")
+            ],
+            linkerSettings: [
+                .linkedFramework("Accelerate")
+            ]
         )
+
     ],
     cxxLanguageStandard: .cxx11
 )
